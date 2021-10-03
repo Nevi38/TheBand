c48908e1bb660a60d0c5ac9f34af6c43fc84d420
tbquocthang <32604240+quocthangngoc14@users.noreply.github.com>
2021-10-03 08:14:27 +0700

    upload

---
+++ assets/js/script-mobile.js
@@ -0,0 +1,33 @@
+var header = document.getElementById('header');
+var mobileMenu = document.getElementById('mobile-menu');
+
+// lấy height của thẻ heaeder
+const headerHeight = document.querySelector('#header').offsetHeight;
+
+// hoặc có thể dùng : const headerHeight = header.clientHeight
+
+// đòng và mở menu, nếu is closed = true thì set height của thẻ header là auto và ngược lại
+// header.clientHeight ==== headerHeight:  return "true"
+mobileMenu.onclick = function () {
+    var isCLosed = header.clientHeight === headerHeight;
+
+    if (isCLosed) {
+        header.style.height = 'auto';
+    } else {
+        header.style.height = null;
+    }  
+}
+
+// Tự động đóng khi chọn menu
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    
+    menuItem.onclick = function () {
+        header.style.height = null;
+    }
+}
+
+// this là chính nó =)))
+// không hiểu là do chưa dành nhiều thời gian cho nó =)) dống cua gái đó
